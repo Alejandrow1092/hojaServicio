de07bd03d8abdad4e899e59ca960469cc3b6b5a7
Alejandro Martínez <amrkratos@hotmail.com>
2020-09-24 19:03:47 -0500

    subi la clase inicio junto con la tabla

--- inicio.php
+++ inicio.php
@@ -4,53 +4,83 @@
         <title>Inicio</title>
 
         <meta name="viewport" content="widht=device-widht, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
+         
+        <!--JQUERY-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <link href="static/css/inicio.css" type="text/css" rel="stylesheet">
-        <script src="static/javascript/index.js"></script>
-         <!-- Los iconos tipo Solid de Fontawesome-->
-         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
-         <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+        
+        <!-- FRAMEWORK BOOTSTRAP para el estilo de la pagina-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
+        <!-- Los iconos tipo Solid de Fontawesome-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+        <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+        <!-- Nuestro css-->
+        <link rel="stylesheet" type="text/css" href="static/css/inicio.css" th:href="@{/css/index.css}">
+        
     </head>
-
-
+    
+            
 
     <body>
 
-        <div class="modal-dialog">
-            <!--Clase del encabezado de la pagina-->
-            <header class="header">
-                   
-                    <div class="container" onclick="myFunction(this)">
-                        <div class="bar1"></div>
-                        <div class="bar2"></div>
-                        <div class="bar3"></div>
+        <div class="top">
+            <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #682444;">
+                <a class="navbar-brand" href="#">
+                    <img src="static/imagenes/escudo.jpg" widht="40" height="40">
+                </a>
+
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="menu">
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                        <a class="nav-link" href="#">Crear Servicio <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Configuracion de usuario <span class="sr-only">(current)</span></a>
+                            </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Cerrar sesion</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    
                     </div>
-                    <h4>UDI</h4>
-                    
-                    
                 </div>
-               
-            </header>
+              </nav>
             
+        </div>
 
-            <!--Clase de la parte central de la pagina-->
-            <div>
+        <div class="content-group text-center">
+            <div class="col-sm-8 main-section">
+                <div class="modal-content" id="modal">
+                    <table class="tabla">
+                        <thead id="tHeader">
+                            <tr>
+                                <th scope="col">Folio</th>
+                                <th scope="col">Solicitante</th>
+                                <th scope="col">Estado</th>
+                            </tr>
 
+                        </thead>
 
-            </div>
+                        <tbody>
 
-            <!--Clase del menu de la pagina-->
-            <div class="menu">
-                <div class="sidebar">
-                    <a ><i class="fas fa-user-cog"></i>Configuracion de usuario</a>
-                    <a><i class="fas fa-user-clock"></i>Crear Servicio</a>
-                    <a>Cerrar sesion</a>
+                        </tbody>
+                    </table>
                 </div>
-                
-                   
-                
             </div>
+
+            
         </div>
 
     </body>
